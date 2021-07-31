0938911c1a5040330d244f950387e9df229fd685
dannyr22 <dannyr2763@hotmail.com>
2021-07-31 17:19:41 +0100

    initial commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,45 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import logo from "../images/letsstaylogonoBackground.png"
+import * as styles from "./header.module.css"
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
+      background: `white`,
     }}
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+    <div class={styles.navBar}>
+      <div class={styles.logo}>
+        <div class={styles.logoWrapper}>
+          <div class={styles.logoContainer}>
+            <img src={logo} />
+          </div>
+        </div>
+      </div>
+      <div class={styles.navLinks}>
+        <ul class={styles.navList}>
+          <li class={styles.navLink}>
+            <Link to="/"></Link>What We Do
+          </li>
+          <li class={styles.navLink}>
+            <Link to="/"></Link>Cocktail List
+          </li>
+          <li class={styles.navLink}>
+            <Link to="/"></Link>Events
+          </li>
+          <li class={styles.navLink}>
+            <Link to="/"></Link>Reviews
+          </li>
+          <li class={styles.navLink}>
+            <Link to="/"></Link>FAQ's
+          </li>
+          <li class={styles.navLink}>
+            <Link to="/"></Link>Contact Us
+          </li>
+        </ul>
+      </div>
     </div>
   </header>
 )

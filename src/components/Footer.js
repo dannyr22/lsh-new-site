f3b2890c3c3086a6b94db530a2bc5ce4104752e2
Danny Roberts <dannyroberts.b@gmail.com>
2021-12-03 09:41:49 +0000

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,15 +4,18 @@ import * as styles from "src/components/Footer.module.css"
 const Footer = () => {
   return (
     <div className={styles.footerContainer}>
-      <div className={styles.firstSide}>
-        <div className={styles.info}>
-          <p>Let's Stay Home Ltd,</p>
-          <p>Sutton,</p>
-          <p>London,</p>
-          <p>email: letsstayhome75@gmail.com</p>
+      <div className={styles.footerMain}>
+        <div className={styles.firstSide}>
+          <div className={styles.info}>
+            <p>Let's Stay Home Ltd,</p>
+            <p>Sutton,</p>
+            <p>London,</p>
+            <p>email: letsstayhome75@gmail.com</p>
+          </div>
         </div>
+        {/* <div className={styles.secondSide}>This is footer two</div> */}
       </div>
-      {/* <div className={styles.secondSide}>This is footer two</div> */}
+      <p style={{ textAlign: "center" }}>&copy; Let's Stay Home Drinks</p>
     </div>
   )
 }
